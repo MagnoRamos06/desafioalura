06b67596cda92fc24c4bd9d32c387794528eb778
MagnoRamos <magnoramos0306@gmail.com>
2025-03-08 16:31:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let amigos = [];
+const listaAmigos = document.getElementById("resultado");
+const loading = document.getElementById("loading"); 
+
+const adicionarAmigo = () => {
+  const amigo = document.getElementById("amigo");
+
+  if (amigo.value != "") {
+    amigos.push(amigo.value);
+    amigo.value = "";
+  } else {
+    alert("Por favor, insira um nome.");
+  }
+
+  atualizarListaAmigos();
+};
+
+const atualizarListaAmigos = () => {
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    listaAmigos.innerHTML += `<li>${amigo}</li>`;
+  });
+};
+
+const sortearAmigo = () => {
+  if (amigos.length > 0) {
+ 
+    loading.style.display = "block";
+    listaAmigos.style.display = "none";
+
+    setTimeout(() => {
+      const min = 0;
+      const max = amigos.length;
+      const indiceAleatorio = Math.floor(Math.random() * (max - min) + min);
+
+      loading.style.display = "none";
+
+      listaAmigos.style.display = "block";
+      listaAmigos.innerHTML = `<li>O amigo sorteado foi: ${amigos[indiceAleatorio]}</li>`;
+    }, 3000);
+  }
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+<h1 align="center"> Desafio Amigo Secreto!</h1>
